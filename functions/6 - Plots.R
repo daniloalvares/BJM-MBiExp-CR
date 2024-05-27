0b1539e2aa222bd34f44267478ec05cf1f675829
Danilo Alvares <daniloalvares@users.noreply.github.com>
2024-05-27 18:37:50 +0100

    Update 6 - Plots.R

--- functions/6 - Plots.R
+++ functions/6 - Plots.R
@@ -3,8 +3,8 @@ plot_avg_trajectory <- function(fit_JM1, fit_JM2, fit_JM3, fit_JM4,
                                 fit_LONG1_M, fit_LONG2_M, fit_LONG3_M, fit_LONG4_M,
                                 fit_LONG1_F, fit_LONG2_F, fit_LONG3_F, fit_LONG4_F){
   
-  dta_JM <- avg_trajectory(fit1=fit_JM1$fit, fit2=fit_JM2$fit, fit3=fit_JM3$fit, fit4=fit_JM4$fit, approach="JE")
-  dta_TS <- avg_trajectory(fit1=fit_LONG1_M$fit, fit2=fit_LONG2_M$fit, fit3=fit_LONG3_M$fit, fit4=fit_LONG4_M$fit, 
+  dta_JM <- avg_trajectory_fc(fit1=fit_JM1$fit, fit2=fit_JM2$fit, fit3=fit_JM3$fit, fit4=fit_JM4$fit, approach="JE")
+  dta_TS <- avg_trajectory_fc(fit1=fit_LONG1_M$fit, fit2=fit_LONG2_M$fit, fit3=fit_LONG3_M$fit, fit4=fit_LONG4_M$fit, 
                            fit5=fit_LONG1_F$fit, fit6=fit_LONG2_F$fit, fit7=fit_LONG3_F$fit, fit8=fit_LONG4_F$fit, approach="TS")
   
   cols <- c("JE"="black", "TS"="gray")
@@ -23,4 +23,13 @@ plot_avg_trajectory <- function(fit_JM1, fit_JM2, fit_JM3, fit_JM4,
 }
 
 
+# INDIVIDUAL WEIGHTED RESIDUALS (IWRES)
+plot_iwres <- function(data1, data2, data3, data4,
+                       fit_JM1, RE_JM1, fit_JM2, RE_JM2, fit_JM3, RE_JM3, fit_JM4, RE_JM4,
+                       fit_LONG1_M, fit_LONG1_F, RE_TS1, fit_LONG2_M, fit_LONG2_F, RE_TS2,
+                       fit_LONG3_M, fit_LONG3_F, RE_TS3, fit_LONG4_M, fit_LONG4_F, RE_TS4){
+
+
+  
+}
 
